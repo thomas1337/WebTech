f637c4511cb0b0e9eb5ab4984bf8d222ae920d71
Malte Neuss <malte.neuss@aol.com>
2014-11-25 12:42:32 +0100

    getMovies should work on php

--- cinemaPHP/PHP/cinemas.php
+++ cinemaPHP/PHP/cinemas.php
@@ -5,12 +5,21 @@
  * Date: 24/11/14
  * Time: 18:18
  */
+ 
+ $DSN = "sqlite:cinam.s3db");
+ $db = new PDO($DSN);
+ $reservedSeatsStatement= $db->prepare("SELECT seat FROM seats WHERE movie_id = :movieId");
 
+//client requests list of reserved seats as json object
+if( isset($_GET["movie"])){
 
-
-if( $_GET["movie"])
-{
-    $data = array(1, 2, 3, 4,5);
+    $data = array(1,2,3);//testdummydata
+    $reservedSeatsStatement->bindParam(":movieId", $_GET["movie"]);
+    if($reservedSeatsStatement->execute(){
+      while($seatNum = $reservedSeatsStatement->fetch()){
+        array_push($data$sea, $seatNum);
+      }
+    }
     header('Content-Type: application/json');
     echo json_encode($data);
 
