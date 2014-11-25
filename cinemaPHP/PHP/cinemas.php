65ecee6b0493edfa1e04a115ed7b62a176c8a2cf
evgenij1987 <avstein@hotmail.de>
2014-11-26 00:22:23 +0100

    todo added

--- cinemaPHP/PHP/cinemas.php
+++ cinemaPHP/PHP/cinemas.php
@@ -28,10 +28,7 @@ if( $_SERVER["REQUEST_METHOD"] == "GET" and isset($_GET["movie"])){
     
     //client posts the reservation data
 } else if($_SERVER["REQUEST_METHOD"] == "POST"){
-    //row id will be automatically added
-    //$reservationId=3;
-
-
+    
     $input= json_decode(file_get_contents('php://input'));
     $seats = $input->seats;
     foreach($seats as $seat){
@@ -50,6 +47,9 @@ if( $_SERVER["REQUEST_METHOD"] == "GET" and isset($_GET["movie"])){
     }
      //give resId back to client for success message
     exit("Die Reservierung war erfolgreich. Ihre ReservierungsID lautet: ".hash("md5", $input->email+$input->movieId));
+    //TODO send back not only registration ID but again the array with all reserved seats,
+    //so the user cannot register twice for the same seats
+    //use JSON of course
 }else{
     header("HTTP/1.0 404 Not Found");
 
